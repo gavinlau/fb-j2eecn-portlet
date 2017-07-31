547b832ddcbf07d01954c222b887827a95253234
java中国 <logicbiz@qq.com>
2017-07-31 10:25:27 +0800

    first commit the fb portlet

---
+++ docroot/WEB-INF/src/com/j2eecn/fb/FeebBackPortlet.java
@@ -0,0 +1,11 @@
+package com.j2eecn.fb;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class FeebBackPortlet
+ */
+public class FeebBackPortlet extends MVCPortlet {
+ 
+
+}
